ccff09db802ca6aed16f2ce9fc31aea59051afee
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-07 16:25:07 +0200

    Scomposizione logica dei passaggi che andrò a fare

---
+++ Mail/main.js
@@ -0,0 +1,10 @@
+//Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+//selezione gli elementi dal mio DOM
+
+//creo la mia lista di e-mail che hanno accesso alla mia pagina
+
+//creo in ciclo di controllo della e-mail inserita dall'utente
+
+// stampo in pagina se l'utente ha accosso o meno alla pagina
+
