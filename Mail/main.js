eae177a14e86607f166e04a49900a4335b8da5a8
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-08 16:42:16 +0200

    Creo il prompt di inserimento della e-mail

--- Mail/main.js
+++ Mail/main.js
@@ -1,23 +1,17 @@
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
+// creo il prompt per la richiesta della e-mail del
 
+let yourEmail = prompt ("Insert your e-mail?");
+console.log (yourEmail);
+document.querySelector(".mail").innerHTML= yourEmail;
+
+//creo la mia lista di e-mail che hanno accesso alla mia pagina*/
 
 const iscritti = ["lorem@gmail.com", "lorem@hotmail.it", "dolorem.est@gmail.it", "ciccio.pasticcio@live.it", "fragola86@outlook.it", "banana33@libero.it", "mario.rossi@rrr.it", "estate24@forse.com"];
-//creo un ciclo per vedere la mia lista 
+//creo un ciclo per vedere la mia lista
 for (let i = 0; i < iscritti.length; i++) {
 
     console.log(iscritti[i]);
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
