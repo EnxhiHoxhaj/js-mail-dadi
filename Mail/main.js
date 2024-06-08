4356a3c9b4f828d7eecad2e1e5c2d304746c44b8
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-08 16:17:09 +0200

    Nuovo approccio esercizio Mail

--- Mail/main.js
+++ Mail/main.js
@@ -1,34 +1,23 @@
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
-let yourEmail = prompt ("Insert your e-mail?");
-console.log (yourEmail);
-document.querySelector(".mail").innerHTML= yourEmail;
 
-//selezione il bottone dal DOM
-
-const myButton = document.querySelector("button");
-
-//creo la mia lista di e-mail che hanno accesso alla mia pagina
 
 const iscritti = ["lorem@gmail.com", "lorem@hotmail.it", "dolorem.est@gmail.it", "ciccio.pasticcio@live.it", "fragola86@outlook.it", "banana33@libero.it", "mario.rossi@rrr.it", "estate24@forse.com"];
+//creo un ciclo per vedere la mia lista 
+for (let i = 0; i < iscritti.length; i++) {
 
-console.log(iscritti);
+    console.log(iscritti[i]);
 
-//gestione evento invio
+}
 
-myButton.addEventListener("click",
-    // al click il programma cerca se la email è tra gli elementi array
-    function () {
-        for (let i = 0; i < iscritti.length; i++){
-            const printRis = document.querySelector(".risultato");
-            //se è tra gli elementi dai il benvenuto
-        } if (yourEmail === iscritti.length) {
-            printRis= "benventui";
-        } else {
-            printRis = "Accesso negato";
-        }
-        console.log(printRis);
-    }
-);
-// stampo in pagina se l'utente ha accosso o meno alla pagina
+
+
+
+
+
+
+
+
+
+
 
