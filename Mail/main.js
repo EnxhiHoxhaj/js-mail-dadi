04f41c8d8264b63b4e685f8ff4d7cd54a5e23ade
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-10 00:08:24 +0200

    Mail: aggiunta di add event listener click

--- Mail/main.js
+++ Mail/main.js
@@ -15,13 +15,19 @@ let printRisultato;
 const iscritti = ["lorem@gmail.com", "lorem@hotmail.it", "dolorem.est@gmail.it", "ciccio.pasticcio@live.it", "fragola86@outlook.it", "banana33@libero.it", "mario.rossi@rrr.it", "estate24@forse.com"];
 
 //creo un ciclo per vedere la mia lista
-for (let i = 0; i < iscritti.length; i++) { 
-    console.log(iscritti[i]);
-    if (iscritti[i] === yourEmail) { // se la e-mail del visitatore è nell'archivio iscritti si concede l'accesso
-        printRisultato = "benvenuta";
-    } else  { //altrimenti si nega l'accesso
-        printRisultato = "Accesso negato";
+myButton.addEventListener ("click", 
+    function () {
+        for (let i = 0; i < iscritti.length; i++) { 
+            console.log(iscritti[i]);
+            if (yourEmail === iscritti[i]) { // se la e-mail del visitatore è nell'archivio iscritti si concede l'accesso
+                printRisultato = "benvenuta";
+            } else  { //altrimenti si nega l'accesso
+                printRisultato = "Accesso negato";
+            }
+            console.log(printRisultato);
+        }; 
     }
-    console.log(printRisultato);
-}; 
+    
+)
+
 
